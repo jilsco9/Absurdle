65da39c4a1a3fcd38f254118eb9a268dfabc16ec
Jillian Scott <jilsco9@gmail.com>
2023-10-11 18:37:36 -0400

    Initial Commit

---
+++ Absurdle/AbsurdleApp.swift
@@ -0,0 +1,32 @@
+//
+//  AbsurdleApp.swift
+//  Absurdle
+//
+//  Created by Jillian Scott on 10/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AbsurdleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AbsurdleUITests/AbsurdleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AbsurdleUITestsLaunchTests.swift
+//  AbsurdleUITests
+//
+//  Created by Jillian Scott on 10/11/23.
+//
+
+import XCTest
+
+final class AbsurdleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
